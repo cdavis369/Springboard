a33f316581b7119086c34989b6a67cc7168a1911
cdavis369 <44884866+cdavis369@users.noreply.github.com>
2023-02-06 13:30:36 -0500

    Add files via upload

---
+++ First Project/timers.js
@@ -0,0 +1,29 @@
+function countdown(n) {
+  let func = function() {
+    n--;
+    if (n === 0) {
+      console.log("DONE!");
+      clearInterval(timer);
+    } else {
+      console.log(n);
+    }
+  }
+  let timer = setInterval(func, 1000);
+}
+  
+function randomGame() {
+  let count = n = 0;
+  let func = function() {
+    count++;
+    n = Math.random().toFixed(2);
+    if (n >= 0.75) {
+      console.log(`${count} tries to reach ${n}`);
+      clearInterval(timer);
+    } else
+    console.log(`${count} tries to reach ${n}`);
+  }
+  let timer = setInterval(func, 1000);
+}
+
+countdown(5);
+randomGame();
