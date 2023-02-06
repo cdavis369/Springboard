3e5a179ec797b3224813c36df1a99c840b0b5db5
cdavis369 <44884866+cdavis369@users.noreply.github.com>
2023-02-06 13:34:32 -0500

    Update timers.js

--- First Project/timers.js
+++ First Project/timers.js
@@ -18,8 +18,9 @@ function randomGame() {
     n = Math.random().toFixed(2);
     if (n >= 0.75) {
       clearInterval(timer);
-    } else
-    console.log(`${count} tries to reach ${n}`);
+    } 
+    else
+      console.log(`${count} tries to reach ${n}`);
   }
   let timer = setInterval(func, 1000);
 }
