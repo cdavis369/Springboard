b2a6bf0ebaed229825cc47714a72d6cac3cb16d0
cdavis369 <44884866+cdavis369@users.noreply.github.com>
2023-02-23 15:52:59 -0500

    Add files via upload

---
+++ ES2015/let-const.js
@@ -0,0 +1,11 @@
+/* 
+
+var PI = 3.14
+const PI = 3.14
+
+let cannot be redeclared nad is scoped by block, not function.
+let can be reassigned while const cannot.
+
+Hoisting: the compiler reads variables before code execution - accessing a var variable before a value is assigned will result in 'undefined' instead of a reference error
+
+*/
