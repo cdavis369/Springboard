de19d7052ee6113cc19dd8efdaedb54d5c54ef26
cdavis369 <44884866+cdavis369@users.noreply.github.com>
2023-02-23 16:14:52 -0500

    Update arrows.js

--- ES2015/arrows.js
+++ ES2015/arrows.js
@@ -1,21 +1,21 @@
-function double(arr) {
-  return arr.map(function(val) {
-    return val * 2;
-  });
-}
+// function double(arr) {
+//   return arr.map(function(val) {
+//     return val * 2;
+//   });
+// }
 
 const double = (arr) => arr.map((val) =>  val * 2);
 
-function squareAndFindEvens(numbers){
-  var squares = numbers.map(function(num){
-    return num ** 2;
-  });
-  console.log(squares);
-  var evens = squares.filter(function(square){
-    return square % 2 === 0;
-  });
-  console.log(evens)
-  return evens;
-}
+// function squareAndFindEvens(numbers){
+//   var squares = numbers.map(function(num){
+//     return num ** 2;
+//   });
+//   console.log(squares);
+//   var evens = squares.filter(function(square){
+//     return square % 2 === 0;
+//   });
+//   console.log(evens)
+//   return evens;
+// }
 
 const squareAndFindEvens = (numbers) => numbers.map((num) => num ** 2).filter((square) => square % 2 === 0);
