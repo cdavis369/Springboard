e48c3e01a5aeeeebf676c7a4c5daa87d0ed6e74c
cdavis369 <44884866+cdavis369@users.noreply.github.com>
2023-03-10 12:16:18 -0500

    Update read_file_list.py

--- fs_5_read_file_list/read_file_list.py
+++ fs_5_read_file_list/read_file_list.py
@@ -19,7 +19,7 @@ def read_file_list(filename):
     # hint: when you read lines of files, there will be a "newline"
     # (end-of-line character) at the end of each line, and you want to
     # strip that off before you print it. Do some research on that!
-    file = open(f"..\{filename}.txt", "r")
+    file = open(f"{filename}.txt", "r")
     for line in file:
         print(f"line = {line}")
 read_file_list("dog")
